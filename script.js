e62e49e79800a39dc54bb39ea02753f53fc9d60f
Md Robiul Islam Nishad <mdnishad119131@gmail.com>
2025-08-20 14:41:11 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Reveal sections on scroll
+const reveals = document.querySelectorAll('.reveal');
+
+window.addEventListener('scroll', () => {
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const revealTop = reveals[i].getBoundingClientRect().top;
+    const revealPoint = 120;
+
+    if (revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add('active');
+    }
+  }
+});
